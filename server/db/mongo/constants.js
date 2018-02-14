01d0dca503dbadff4ff849679cac36563d8ddaaf
Soham Nakhare <sohamnakhare@users.noreply.github.com>
2018-02-14 11:02:41 +0530

    Update constants.js

--- server/db/mongo/constants.js
+++ server/db/mongo/constants.js
@@ -1,4 +1,4 @@
-export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost/ReactWebpackNode';
+export const db = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost/ReactWebpackNode';
 
 export default {
   db
